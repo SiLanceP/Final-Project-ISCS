2515c1103122ae6299cd3db49b3010fc5045fd3e
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-11-21 13:23:53 +0800

    Added documentation and organized code to follow Solidity code conventions

--- Final Project/FinalProject_Contract.sol
+++ Final Project/FinalProject_Contract.sol
@@ -8,44 +8,66 @@ If any Solidity language code or documentation used in our program was obtained 
 // Decentralized file sharing system using IPFS hashes
 pragma solidity ^0.8.27;
 
+/// @title Peer-to-Peer File Sharing System
+/// @author Shaan Graal Dayag, Jez Deign Gonzales, Bea Montaño, Lance Pequierda, Jeanne Regala
+/// @notice System for managing files in an academic setting
+ 
+/// @notice Contract for the system
 contract P2PFileShare{
+    /// @notice Base URL (to be used with the IPFS hash)
+    /// @dev (Copy this url + the hash and paste it in browser to grab the file)
+    /// @dev (No need to download IPFS desktop)
+    string constant BASE_GATEWAY = "https://ipfs.io/ipfs/";
 
-    struct FileInfo {
-        uint idnumber;
-        string ipfs; //file location on IPFS (gets the hash)
-        string subject; // "CSCI 101.14" / "THEO 12" / "MSYS 41" 
-        string fileName;        // "Assignment_1.pdf"
-        address uploader;       // Who posted it (Teacher/Student)
-    }
-
-    //mapping to store files (it acts like a hash table/dictionary
-    mapping(uint256 => FileInfo) public files;
+    /// @notice fileCount   number of files in the system
     uint256 public fileCount = 0;
 
-    //Base URL (to be used with the ipfs hash) (just copy this url + the hash and paste it on browser to grab the file) (no need to download ipfs desktop)
-    string constant baseGateway = "https://ipfs.io/ipfs/";
+    /// @notice Structure representing metadata information of the file being shared
+    /// @notice idnumber    ID Number (either employee ID or student ID depending on uploader role)
+    /// @notice ipfs        hash of the file location on IPFShash of the file location on IPFS
+    /// @notice subject     official subject code for the subject the file is for (ex: "CSCI 101.14",
+    ///                     "THEO 12", "MSYS 41")
+    /// @notice fileName    name of the file including the filetype extension (ex: "Assignment_1.pdf")
+    /// @notice uploader    address of the uploader
+    struct FileInfo {
+        uint idnumber;
+        string ipfs;
+        string subject;
+        string fileName;
+        address uploader;
+    }
 
-    //Event to notify when a file is uploaded
+    /// @notice Mapping that stores files by acting like a hash table/dictionary
+    mapping(uint256 => FileInfo) public files;
+
+    /// @notice Event that notifies upon successful file upload
+    /// @param idnumber ID Number (either employee ID or student ID depending on uploader role)
+    /// @param ipfs hash of the file location on IPFS
+    /// @param subject indexed, allows searching by subject course code; official course code for the subject the 
+    ///                file is for (ex: "CSCI 101.14", "THEO 12", "MSYS 41")
+    /// @param fileName name of the file including the filetype extension (ex: "Assignment_1.pdf")
+    /// @param uploader indexed, allows searching by uploader; address of the uploader
     event FileUploaded(
         uint idnumber,
         string ipfs,
-        string indexed subject, // allows searching by Course subject
+        string indexed subject,
         string fileName,
-        address indexed uploader //allows searching by uploader
+        address indexed uploader
     );
-
-
-
-    //Function to upload the file
+    
+    /// @notice Function to upload the file
+    /// @param _ipfs IPFS file location hash for the file
+    /// @param _fileName filename with filetype extension
+    /// @param _subject official course code for the subject that file is being uploaded for
     function uploadFile(string memory _ipfs, string memory _fileName, string memory _subject) public {
         require(bytes(_ipfs).length > 0, "IPFS Hash is required");
         require(bytes(_fileName).length > 0, "File Name is required");
         require(bytes(_subject).length > 0, "Subject is required");
 
-        //this counts the number of files have been uploaded to the contract
+        /// @dev Updates the number of files have been uploaded to the contract
         fileCount++;
 
-        //show the details of the file uploaded
+        /// @dev Populates the details of the file uploaded
         files[fileCount] = FileInfo(
             fileCount,
             _ipfs,
@@ -54,16 +76,18 @@ contract P2PFileShare{
             msg.sender
         );
 
-        //emit is used to trigger the "event"
-        emit FileUploaded(fileCount,_ipfs,_subject,_fileName,msg.sender);
+        emit FileUploaded(fileCount, _ipfs, _subject, _fileName, msg.sender);
     }
 
-    //function on getting the file details
+    /// @notice Function that returns the file details
+    /// @param _idnumber idnumber of the uploader
+    /// @return info metadata from the uploaded file
+    /// @return downloadUrl URL that allows users to download the file
     function getFile(uint _idnumber) public view returns (FileInfo memory info, string memory downloadUrl) {
         FileInfo memory file = files[_idnumber];
 
-        // this makes it possible to just copy the text itself and paste it on a browser
-        string memory fullUrl = string(abi.encodePacked(baseGateway, file.ipfs));
+        /// @dev This makes it possible to just copy the text itself and paste it on a browser
+        string memory fullUrl = string(abi.encodePacked(BASE_GATEWAY, file.ipfs));
 
         return (file, fullUrl);
     }
